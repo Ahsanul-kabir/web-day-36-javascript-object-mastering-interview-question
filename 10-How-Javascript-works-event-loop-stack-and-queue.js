172af2202ea2c10e623351d2394baf22b5c9ff59
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2021-02-07 17:37:28 +0600

    10-How-Javascript-works-event-loop-stack-and-queue added

---
+++ 10-How-Javascript-works-event-loop-stack-and-queue.js
@@ -0,0 +1,19 @@
+// Resource : https://developer.mozilla.org/en-US/docs/Web/JavaScript/EventLoop
+// Video Resource: https://www.youtube.com/watch?v=8aGhZQkoFbQ
+
+// Jascript e jokon amra ekta function ke bha kisu ke call kori tar modde ekta event loop thake
+
+// call hote hote stack create kore = sobar aager tar kaj ses kore then tar porer ta dore
+
+
+// then tar kase time thakle
+
+// setTimeout ekta jaigai wait kore then Queue te ase
+
+// kaj korte korte waiting list theke Queue te ane
+
+// ei khan ekta event loop o thake
+
+
+// == stack wise kaj gula ke rakhe and kajer ekta Queue ase ei Queue theke kaj tan(pull) dei jokon Stack er kaj ses hoye jai.
+// so, Syncronus kaj ses hoibar pore Asyncronus kaj ke dore
