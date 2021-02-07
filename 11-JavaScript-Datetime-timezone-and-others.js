15a9797c8e853923a8fbc5c23ccaf615c502a42a
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2021-02-07 17:56:44 +0600

    11-JavaScript-Datetime-timezone-and-others added

---
+++ 11-JavaScript-Datetime-timezone-and-others.js
@@ -0,0 +1,9 @@
+// new Date();  //= date dekhabe
+
+const start = new Date();
+let sum = 0;
+for (let i = 0; i < 10000000; i++) {
+    sum++;
+}
+const end = new Date();
+console.log('time elapsed', end - start, sum); // end - start = koto somoy lagse for loop colte bahir korsi
